39d202ffe5525c124910715abad29e54c68ff461
Stacy Johns <stacykjohns@icloud.com>
2020-01-28 18:44:15 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,18 +2,25 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
+def batch_badge_creator(attendees)
   batch = []
-  array.each do |name|
-    batch << badge_maker(name)
+  attendees.each do |attendee|
+    batch << badge_maker(attendee)
   end
   batch
 end
 
-def assign_rooms(array)
-  array.each_with_index do |name, index|
-    index = 1
-    "Hello, #{name}! You'll be assigned to room #{index}"
-    index += 1
+def assign_rooms(attendees)
+  assigned = []
+  attendees.each.with_index(1) do |attendee, index|
+    assigned << "Hello, #{attendee}! You'll be assigned to room #{index}!"
+  end
+  assigned
+end
+
+def printer(attendees)
+  printer_list = batch_badge_creator(attendees).concat assign_rooms(attendees)
+  printer_list.each do |line|
+    puts line
   end
 end
