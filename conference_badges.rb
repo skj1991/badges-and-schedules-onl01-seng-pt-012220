30f59a3680fa21dea7514d5ed2c121891b57281d
Stacy Johns <stacykjohns@icloud.com>
2020-01-28 18:36:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,8 +12,8 @@ end
 
 def assign_rooms(array)
   assigned = []
-  array.each_with_index(1) do |name, index|
-    assigned << "Hello, #{name}! You'll be assigned to room #{index}!"
+  array.each_with_index do |name, index|
+    assigned << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   end
   assigned
 end
