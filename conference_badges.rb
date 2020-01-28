1d2e2cfc5b0e352085d29c014c6ecc869ae5f189
Stacy Johns <stacykjohns@icloud.com>
2020-01-28 16:22:21 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  batch = []
+  array.each do |name|
+    batch << badge_maker(name)
+  end
+  batch
+end
+
+def assign_rooms(array)
+  array.each_with_index do |name, index|
+    index = 1
+    "Hello, #{name}! You'll be assigned to room #{index}"
+    index += 1
+  end
+end
